6c1a64cdb132ed4991ae6077e33a65e5c6d489ee
dtklein <david@tenar.us>
2017-04-13 13:12:59 -0500

    Pull network info into one place
    First upload. This is just pulling interfaces and routes, that's it.

---
+++ Network_Configs.ps1
@@ -0,0 +1,13 @@
+﻿
+[System.Collections.ArrayList]$ifaces=New-Object -TypeName System.Collections.ArrayList ;
+[System.Collections.ArrayList]$gateways=New-Object -TypeName System.Collections.ArrayList ;
+$NWConfig=@{} ;
+
+Get-NetIPAddress |select InterfaceAlias,IPv4Address |Where-Object { $_.IPv4Address -notmatch "^((127|169)\.|$)" } |% {$ifaces.Add($_)} 
+Get-NetRoute -DestinationPrefix 0.0.0.0/0 |select InterfaceAlias,NextHop |% { $gateways.Add($_) } 
+
+$NWConfig.Add("Interfaces",$ifaces) 
+$NWConfig.Add("Gateways",$gateways)
+
+$NWConfig |ConvertTo-Json |Write-Output 
+
