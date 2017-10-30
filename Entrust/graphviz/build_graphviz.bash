6987b76a5c928364105f21498c669806bf642712
David T. Klein <david.klein@hush.com>
2017-10-29 21:05:48 -0500

    Updated Microsoft Office Online to be in its own menu Created a deb to give dektop-like launchers for Apple iCloud pushing Entrust certs

--- Entrust/graphviz/build_graphviz.bash
+++ Entrust/graphviz/build_graphviz.bash
@@ -2,27 +2,43 @@
 
 echo -e "digraph entrust_trust_web {\n\tlayout=\"dot\"\n\trankdir=\"BT\"">./graph ;
 export i=0 ;
-for FILE in *.cer ; do 
-  SUBJ=$(openssl x509 -in $FILE -noout -subject) ; 
-  ISS=$(openssl x509 -in $FILE -noout -issuer) ; 
-  SUBJ_CN=$(echo ${SUBJ} \
+for FILE in $(ls ../certs/*.{cer,crt}) ; do 
+  SUBJ=$(openssl x509 -in $FILE -noout -subject \
   |awk '
     BEGIN {
       RS="/" ; 
-      FS="="
+      FS="=" ;
+      CN="" ;
+      O="" ;
+      OUTPUT="" ;
     } ; 
     $1 == "CN" {
-      print $2
-    }') ;
-  ISS_CN=$(echo ${ISS} \
+      CN=$2 ;
+    }
+    $1 == "O" {
+      O=$2 ;  
+    }
+    END {
+      printf CN "," O ;
+    }' |tr -d "\n") ;
+  ISS=$(openssl x509 -in $FILE -noout -issuer \
   |awk '
     BEGIN {
       RS="/" ; 
-      FS="="
+      FS="=" ;
+      CN="" ;
+      O="" ;
+      OUTPUT="" ;
     } ; 
     $1 == "CN" {
-      print $2
-    }') ;
-  echo -e "\t\"${SUBJ_CN}\" -> \"${ISS_CN}\"" >>./graph ;
+      CN=$2 ;
+    }
+    $1 == "O" {
+      O=$2 ;  
+    }
+    END {
+      printf CN "," O ;
+    }' |tr -d "\n") ;
+  echo -e "\t\"${SUBJ}\" -> \"${ISS}\"" >>./graph ;
 done
 echo -e "}" >>./graph
